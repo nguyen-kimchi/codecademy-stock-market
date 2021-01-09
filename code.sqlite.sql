661cc5c3c014c224377470a3a283f51481ea2fec
Kim Nguyen <56841825+nguyen-kimchi@users.noreply.github.com>
2021-01-09 17:30:55 +0000

    Added additional data for Apple and Domino's stock

--- code.sqlite.sql
+++ code.sqlite.sql
@@ -1,4 +1,4 @@
---Manipulation: Creating table 
+-- Manipulation: Creating table 
 
 CREATE TABLE stocks (
 	symbol TEXT,
@@ -6,7 +6,7 @@ CREATE TABLE stocks (
 	datetime DATETIME,
 	price INTEGER);
 
---Manipulation: Collecting stock data and inserting into table
+-- Manipulation: Collecting stock data and inserting into table
 
 INSERT INTO stocks (symbol, name, datetime, price)
 VALUES
@@ -53,6 +53,7 @@ VALUES
 	("+", "Zoom Video Communications, Inc. (ZM)", "2020-08-03 21:00:00", 268.00),
 	("+", "Zoom Video Communications, Inc. (ZM)", "2020-07-01 21:00:00", 259.13);
 
+
 -- Basic Requirement: 1. What are the distinct stocks in the table?
 
 SELECT DISTINCT name 
@@ -61,7 +62,7 @@ FROM stocks;
 /* Returned result with name column. Table contained 5 rows of the name of distinct stock: 
 Apple, Domino's, easyJet, Rolls-Royce and Zoom */
 
---Basic Requirement: 2. Query all data for a single stock. Do you notice any overall trends?
+-- Basic Requirement: 2. Query all data for a single stock. Do you notice any overall trends?
 
 SELECT * 
 FROM stocks
@@ -69,7 +70,7 @@ WHERE name = "Apple Inc. (AAPL)";
 
 /* Not enough data to identify trend. However, there seems to be a three-month cycle.*/
 
---Basic Requirement: 3. Which rows have price above 100? between 40-50, etc?
+-- Basic Requirement: 3. Which rows have price above 100? between 40-50, etc?
 
 SELECT * FROM stocks
 WHERE price > 100;
@@ -93,7 +94,7 @@ SELECT name, MAX(price) FROM stocks;
 SELECT name, MIN(price) FROM stocks;
 -- This returns min price with name 
 
---Intermediate Challenge: 1 and 2. Look at key statistics about data and group by stock
+-- Intermediate Challenge: 1 and 2. Look at key statistics about data and group by stock
 
 SELECT name, 
 	MAX(price), MIN(price), AVG(price)
@@ -102,4 +103,37 @@ GROUP BY name
 ORDER BY name;
 -- Looks at the min, max and average closing price for each stock 
 
+-- Intermediate Challenge: 3. Group the data by day or hour of day. Does day of week or time of day impact prices?
 
+INSERT INTO stocks 
+VALUES
+	("-", "Apple Inc. (AAPL)", "2020-12-31 21:00:00", 132.69),
+	("+", "Apple Inc. (AAPL)", "2020-12-15 21:00:00", 127.88),
+	("+", "Apple Inc. (AAPL)", "2020-11-30 21:00:00", 119.05),
+	("+", "Apple Inc. (AAPL)", "2020-11-16 21:00:00", 120.30),
+	("-", "Apple Inc. (AAPL)", "2020-10-30 21:00:00", 108.86),
+	("-", "Apple Inc. (AAPL)", "2020-10-15 21:00:00", 120.71),
+	("+", "Apple Inc. (AAPL)", "2020-09-30 21:00:00", 115.81),
+	("+", "Apple Inc. (AAPL)", "2020-09-15 21:00:00", 115.54),
+	("+", "Apple Inc. (AAPL)", "2020-08-31 21:00:00", 129.04),
+	("-", "Apple Inc. (AAPL)", "2020-08-17 21:00:00", 114.61),
+	("+", "Apple Inc. (AAPL)", "2020-07-31 21:00:00", 106.26),
+	("+", "Apple Inc. (AAPL)", "2020-07-15 21:00:00", 97.72),
+	("-", "Domino's Pizza (DPZ)", "2020-12-31 21:00:00", 383.46),
+	("+", "Domino's Pizza (DPZ)", "2020-12-15 21:00:00", 384.68),
+	("+", "Domino's Pizza (DPZ)", "2020-11-30 21:00:00", 392.57),
+	("-", "Domino's Pizza (DPZ)", "2020-11-16 21:00:00", 384.82),
+	("-", "Domino's Pizza (DPZ)", "2020-10-30 21:00:00", 378.32),
+	("-", "Domino's Pizza (DPZ)", "2020-10-15 21:00:00", 401.84),
+	("+", "Domino's Pizza (DPZ)", "2020-09-30 21:00:00", 425.28),
+	("+", "Domino's Pizza (DPZ)", "2020-09-15 21:00:00", 389.52),
+	("-", "Domino's Pizza (DPZ)", "2020-08-31 21:00:00", 408.96),
+	("+", "Domino's Pizza (DPZ)", "2020-08-17 21:00:00", 309.39),
+	("+", "Domino's Pizza (DPZ)", "2020-07-31 21:00:00", 386.61),
+	("+", "Domino's Pizza (DPZ)", "2020-07-15 21:00:00", 413.68),	
+
+-- Adding additional data for Apple and Domino's stocks
+
+
+	
+	
